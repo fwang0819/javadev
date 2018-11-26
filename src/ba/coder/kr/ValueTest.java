dfcd3fec4d383fca66a2ad4981275632551c661f
bacoder <bacoder@DESKTOP-SU34KMU>
2018-11-26 21:54:45 +0900

    init

---
+++ src/ba/coder/kr/ValueTest.java
@@ -0,0 +1,37 @@
+package ba.coder.kr;
+
+public class ValueTest {
+int globalVariable = 5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hellow world");
+		//System.out.println("Ω≈±‚«‘");
+
+		//	int level;
+    //int up;
+      //   up=8;   	
+		//level =10;
+	//	System.out.println(level
+			//	+up);
+		//System.out.println();
+	int aVal=10;
+	int bVal=5;
+	int Tot;
+	
+	Tot=aVal+bVal;
+	System.out.println("TOTAL="+Tot);
+	
+	
+	
+	
+	}
+	public void add() {
+		int total =null'
+		
+		int Val2 =5;
+		int total =globalVariable + Val2;
+		
+	}
+
+}
